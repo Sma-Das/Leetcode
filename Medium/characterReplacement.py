3c127e458491fdf36b817616cde8f4960bf298f5
Sma-Das <smaddas10@gmail.com>
2021-05-24 12:48:30 +0400

    characterReplacement.py updated with a for loop version

--- Medium/characterReplacement.py
+++ Medium/characterReplacement.py
@@ -13,3 +13,17 @@ def characterReplacement(s, k):
         max_length = max(max_length, length)
         i += 1
     return max_length
+
+
+def characterReplacement2(s, k):
+    maxLength, s_len = 0, len(s)
+    for i, char in enumerate(s):
+        length, rem = 0, k
+        for j in range(i+1, s_len):
+            if not rem:
+                break
+            if s[j] != char:
+                rem -= 1
+            length += 1
+        maxLength = max(maxLength, length)
+    return maxLength
