d1f44d93db70856836a7ccec69dc2e9056c6e108
Sma-Das <smaddas10@gmail.com>
2021-05-30 17:33:17 +0400

    Cleaned up imports

--- 1-Easy/diStringMatch.py
+++ 1-Easy/diStringMatch.py
@@ -1,4 +1,7 @@
+from collections import deque
+
+
 class Solution:
     def diStringMatch(self, s: str) -> List[int]:
         numbers = deque(range(len(s)+1))
-        return [numbers.popleft() if l  == "I" else numbers.pop() for l in s+"A"]
+        return [numbers.popleft() if l == "I" else numbers.pop() for l in s+"A"]
